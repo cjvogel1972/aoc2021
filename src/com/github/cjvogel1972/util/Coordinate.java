7a48053755a6145a3278b675cf0ca5ea102ee998
Chris Vogel <cjvogel1972@gmail.com>
2021-12-13 15:55:28 -0600

    Remove boilerplate code from switching to a record

--- src/com/github/cjvogel1972/util/Coordinate.java
+++ src/com/github/cjvogel1972/util/Coordinate.java
@@ -1,27 +1,4 @@
 package com.github.cjvogel1972.util;
 
-import java.util.Objects;
-
 public record Coordinate(int x, int y) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coordinate that = (Coordinate) o;
-        return x == that.x && y == that.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Coordinate{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
 }
